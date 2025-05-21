e7edbf8e2d0f01a03a5eb54495317e1bbbeeef97
philippe <philippe@PhAYoga.home>
2025-05-21 22:28:45 +0200

    1.6

--- arbitre/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $_SESSION = array();  // détruit le tableau session
-    // destruction du cookie de session
-    setcookie('biathlon_arbitre_token', '', time() - 3600, '/');
-    session_destroy();
-    header("Location: /arbitre/");
-?>
